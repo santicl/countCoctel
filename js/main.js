f2417ea4895fb63f6a7b908059c2544f9be5599c
unknown <caballerosuarezsantiago@gmail.com>
2022-07-10 14:00:06 -0500

    Up Files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let btn = document.getElementsByClassName("btn");
+
+let doc = [
+    { coctel: 'margarita', count: 0 },
+    { coctel: 'vodka', count: 0 },
+    { coctel: 'whiskey', count: 0 },
+    { coctel: 'whiskeycoca', count: 0 },
+    { coctel: 'gin', count: 0 },
+];
+localStorage.setItem("doc", JSON.stringify(doc));
+
+for (const el of btn) {
+    el.addEventListener("click", function (e) {
+        console.log(e.target);
+        sum(e.target.id);
+    })
+}
+
+const reload = () => {
+    window.location.reload();
+}
+
+const sum = (id) => {
+    let doc = localStorage.getItem("doc");
+    doc = JSON.parse(doc);
+    for (let i = 0; i < doc.length; i++) {
+        if (doc[i].coctel === id) {
+            doc[i].count++;
+        }
+    }
+    localStorage.setItem("doc", JSON.stringify(doc));
+    read();
+};
+
+const read = () => {
+    let doc = localStorage.getItem("doc");
+    let docObj = JSON.parse(doc);
+    for (let i = 0; i < docObj.length; i++) {
+        const { coctel, count } = docObj[i];
+        document.getElementById(coctel + "Count").innerHTML = count;
+    }
+}
+
+document.getElementById("reload").addEventListener("click", reload);
